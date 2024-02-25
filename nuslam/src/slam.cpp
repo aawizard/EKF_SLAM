9a1d81b472f8367da39364b3d6dfe8d703a241c1
aawizard <agwananyaa2001@gmail.com>
2024-02-25 17:51:39 -0600

    Slam code done. Its not working though.

--- nuslam/src/slam.cpp
+++ nuslam/src/slam.cpp
@@ -53,6 +53,7 @@ public:
     declare_parameter("wheel_right", "");
     declare_parameter("wheel_radius", -1.0);
     declare_parameter("track_width", -1.0);
+    declare_parameter("input_noise", 0.001);
 
     body_id = get_parameter("body_id").as_string();
     odom_id = get_parameter("odom_id").as_string();
@@ -60,6 +61,8 @@ public:
     wheel_right = get_parameter("wheel_right").as_string();
     wheel_radius = get_parameter("wheel_radius").as_double();
     track_width = get_parameter("track_width").as_double();
+    input_noise = get_parameter("input_noise").as_double();
+
     if (body_id == "") {
       RCLCPP_ERROR_STREAM(get_logger(), "body_id not set");
       exit(-1);
@@ -78,6 +81,7 @@ public:
       "odom", 10, std::bind(&Slam::odom_callback, this, std::placeholders::_1));
     sub_fake_sensor_ = create_subscription<visualization_msgs::msg::MarkerArray>(
       "fake_sensor", 10, std::bind(&Slam::fake_sensor_callback, this, std::placeholders::_1));
+    pub_estimate_obs_ = create_publisher<visualization_msgs::msg::MarkerArray>("estimate_obs", 10);
 
     // pub_odom_ = create_publisher<nav_msgs::msg::Slam>("odom", 10);
     pub_path_ = create_publisher<nav_msgs::msg::Path>("green/path", 10);
@@ -100,6 +104,7 @@ private:
     // m.getRPY(roll, pitch, yaw);
     turtlelib::Twist2D twist{msg->twist.twist.linear.x, 0, yaw};
     T_del *= turtlelib::integrate_twist(twist);
+    // T_del = turtlelib::Transform2D(turtlelib::Vector2D{twist.x, twist.y}, twist.omega);
     Tob_ = turtlelib::Transform2D(turtlelib::Vector2D{msg->pose.pose.position.x, msg->pose.pose.position.y}, yaw);
     
     // Publish Transform
@@ -133,17 +138,39 @@ private:
     //Subscribing to fake sensor data
     for (int i = 0; i < static_cast<int>(msg->markers.size()); i++){
       turtlelib::Transform2D Tmobs = Tmb_ * turtlelib::Transform2D(turtlelib::Vector2D{msg->markers[i].pose.position.x, msg->markers[i].pose.position.y});
-      state_.subvec(3+2*i, 3+2*i+1) = {Tmobs.translation().x, Tmobs.translation().y};
+      auto r = sqrt(pow(msg->markers[i].pose.position.x,2) + pow(msg->markers[i].pose.position.y,2));
+      auto theta = atan2(msg->markers[i].pose.position.y, msg->markers[i].pose.position.x);
+      z_obs.subvec(2*i, 2*i+1) = {r, theta};
+      // z_obs.subvec(2*(msg->markers[i].id), 2*(msg->markers[i].id)+1) = {Tmobs.translation().x, Tmobs.translation().y};
+      // state_.subvec(3+2*(msg->markers[i].id), 3+2*(msg->markers[i].id)+1) = {Tmobs.translation().x, Tmobs.translation().y};
 
     }
     
     //Variance matrix
-    arma::Mat<double> Sigma = get_pose_covariance(msg->markers.size());
+    set_prior_covariance();
+    //Measurement Model
+    update_measurement_vector();   // z and H are updated
 
+    // Calculate Kalman Gain
+    arma::Mat<double> R = arma::Mat<double>(2*max_obs, 2*max_obs, arma::fill::eye);
+    // R.diag() = arma::randn(2*max_obs);
+    std::normal_distribution<> d(0, input_noise);
+    for(int i;i<2*max_obs;i++){
+      R(i,i) = d(get_random());
+    }
+    // RCLCPP_INFO_STREAM(get_logger(), "\n "<<H);
+    arma::Mat<double> K = Sigma * H.t() * arma::inv(H * Sigma * H.t() + R);
+    // arma::Mat<double> K = Sigma * H.t() * arma::inv(H * Sigma * H.t());
+    
+    //EKF SLAM Step 2 - Update Step
+    state_ += K * (z_obs - z_pred);
+    Sigma = (arma::Mat<double>(3+2*max_obs, 3+2*max_obs, arma::fill::eye) - K * H) * Sigma;
 
+    // Update Transform
+    RCLCPP_INFO_STREAM(get_logger(), state_[1]<<" "<< state_[2] << " "<< state_[0] );
+    Tmb_ = turtlelib::Transform2D(turtlelib::Vector2D{state_[1], state_[2]}, state_[0]);
+    Tmo_ = Tmb_ * Tob_.inv();
     // Publish map to odom transform
-    // Tmb_ = Tmo_ * robot.get_robot_pos();
-    // state_ = {Tmb_.rotation(), Tmb_.translation().x, Tmb_.translation().y};
     map_odom_.header.stamp = this->get_clock()->now();
     map_odom_.transform.translation.x = Tmo_.translation().x;
     map_odom_.transform.translation.y = Tmo_.translation().y;
@@ -154,6 +181,8 @@ private:
     map_odom_.transform.rotation.z = q.z();
     map_odom_.transform.rotation.w = q.w();
     tf_broadcaster_->sendTransform(map_odom_);
+    T_del = turtlelib::Transform2D();
+    publish_estimate_markers();
   }
  
 /// \brief    Function to change quaternion to euler angles and get yaw
@@ -164,48 +193,88 @@ private:
     auto w = quaternion.w;
     auto siny_cosp = 2 * (w * z + x * y);
     auto cosy_cosp = 1 - 2 * (y * y + z * z);
-    double yaw = std::atan2(siny_cosp, cosy_cosp);
-    return yaw;
+    return std::atan2(siny_cosp, cosy_cosp);
   }
 
-  arma::Col<double> get_polar_coordinates(double x, double y){
-    double r = std::sqrt(x*x + y*y);
-    double theta = std::atan2(y, x);
+  arma::Col<double> get_polar_coordinates(double mx, double my){
+    double r = sqrt(pow(mx-state_.at(1),2) + pow(my-state_.at(2),2));
+    double theta = turtlelib::normalize_angle(atan2(my-state_.at(2), mx-state_.at(1)) - state_.at(0));
     return {r, theta};
   }
 
-  arma::Mat<double> get_pose_covariance(int n){
-
-    RCLCPP_INFO_STREAM(get_logger(), "here!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-    arma::Mat<double> Sigma = arma::Mat<double>(3+2*n, 3+2*n, arma::fill::zeros);
-    Sigma.submat(0,0,2,2) = 0.1*arma::eye(3,3);
+  void set_prior_covariance(){
     // Calculating A matrix
-    arma::Mat<double> A = arma::Mat<double>(2*n, 2*n, arma::fill::eye);
-    if(T_del.translation().x != 0.0 || T_del.translation().y != 0.0){
+    arma::Mat<double> A = arma::Mat<double>(3+2*max_obs, 3+2*max_obs, arma::fill::eye);
+    if((T_del.translation().x != 0.0 || T_del.translation().y != 0.0) ){
       if(turtlelib::almost_equal(T_del.rotation(),0)){
-        A(1,0) = -T_del.translation().x / sin(state_[0]);
-        A(2,0)= T_del.translation().x / cos(state_[0]);
+        A(1,0) = -T_del.translation().x * sin(state_[0]);
+        A(2,0)= T_del.translation().x * cos(state_[0]);
       }
       else{
-        A(1,0) = (T_del.translation().x / T_del.rotation()) * (cos(state_[0] + T_del.rotation()) - cos(state_[0]));
-        A(2,0) = (T_del.translation().x / T_del.rotation()) * (sin(state_[0] + T_del.rotation()) - sin(state_[0]));
+        A(1,0) = (T_del.translation().x / T_del.rotation()) * (cos(turtlelib::normalize_angle(state_[0] + T_del.rotation())) - cos(state_[0]));
+        A(2,0) = (T_del.translation().x / T_del.rotation()) * (sin(turtlelib::normalize_angle(state_[0] + T_del.rotation())) - sin(state_[0]));
       }
     }
+    // Make Q matrix
+    arma::Mat<double> Q = arma::join_cols(arma::join_rows(arma::eye(3,3), arma::Mat<double>(3, 2 * max_obs, arma::fill::zeros)),
+                                      arma::Mat<double>(2 * max_obs, 3 + 2* max_obs, arma::fill::zeros));
+    // Q.submat(0,0,2,2).diag() = arma::randn(3);    
+    std::normal_distribution<> d(0, input_noise);
+        Q.submat(0,0,2,2).diag() = arma::vec{d(get_random()), d(get_random()), d(get_random())};    
+    Sigma = A * Sigma * A.t() + Q;
+    // Sigma = A * Sigma * A.t();
     
-    RCLCPP_INFO_STREAM(get_logger(), "A: \n" << A);
-
-
-    for (int i = 0; i < n; i++){
-      Sigma.submat(3+2*i,3+2*i,3+2*i+1,3+2*i+1) = 0.1*arma::eye(2,2);
-    }
-    return Sigma;
   }
-  void cal_h(){
-    int n = (state_.size()-3)/2;
-    for (int i = 0; i < n; i++){
-      
-    }
+
+  void update_measurement_vector(){
+    for (int i=0; i<max_obs;i++){
+      if(state_[1] != 0.0 || state_[2] != 0.0){
+        z_pred.subvec(2*i, 2*i +1) = get_polar_coordinates(state_[3+2*i], state_[3+2*i+1]);
+        auto del_x = state_[3+2*i] - state_[1];
+        auto del_y = state_[3+2*i+1] - state_[2];
+        auto d = pow(del_x,2) + pow(del_y,2);
+        H.submat(2*i, 0, 2*i+1, 2) = arma::join_rows(arma::vec{0,-1}, arma::vec{-del_x/sqrt(d), del_y/d}, arma::vec{-del_y/sqrt(d), -del_x/d}); 
+        H.submat(2*i, 3 + 2*i, 2*i+1, 3 + 2*i+1) = arma::join_rows(arma::vec{del_x/sqrt(d), -del_y/d}, arma::vec{del_y/sqrt(d), del_x/d});
+      }
+    }    
   }
+
+  void publish_estimate_markers(){
+    visualization_msgs::msg::MarkerArray markers;
+    visualization_msgs::msg::Marker marker;
+    for (int i=0; i<max_obs;i++){
+      marker.id = i;
+      if(state_[3+2*i] != 0.0 || state_[3+2*i+1] != 0.0){
+        marker.header.frame_id = "map";
+        marker.header.stamp = this->get_clock()->now();
+        
+        marker.type = visualization_msgs::msg::Marker::CYLINDER;
+        marker.action = visualization_msgs::msg::Marker::ADD;
+        marker.pose.position.x = state_[3+2*i];
+        marker.pose.position.y = state_[3+2*i+1];
+        marker.pose.position.z = wall_height/2;
+        marker.scale.x = 0.1;
+        marker.scale.y = 0.1;
+        marker.scale.z = wall_height;
+        marker.color.a = 1.0;
+        marker.color.g = 1.0;
+      }
+      else{
+        marker.action = visualization_msgs::msg::Marker::DELETE;
+      }
+      markers.markers.push_back(marker);
+    }
+    pub_estimate_obs_->publish(markers);
+  }
+
+   std::mt19937 & get_random()
+  {
+    // static variables inside a function are created once and persist for the remainder of the program
+    static std::random_device rd{};
+    static std::mt19937 mt{rd()};
+    return mt;
+  }
+  
 
   
   std::string body_id = "";
@@ -216,6 +285,9 @@ private:
   double track_width = 0.0;
   double left_wheel_joint = 0.0;
   double right_wheel_joint = 0.0;
+  double input_noise = 0.0;
+  int max_obs = 30;
+  double wall_height = 0.25;
   bool first = true;
   nav_msgs::msg::Odometry odom;
   geometry_msgs::msg::TransformStamped odom_robot_tf_;
@@ -225,13 +297,18 @@ private:
   turtlelib::Transform2D Tmb_;   // Transform from map to base
   turtlelib::Transform2D Tob_;   // Transform from odom to base
   turtlelib::Transform2D T_del = turtlelib::Transform2D();  // Transform from odom to base in time 200ms 
-  arma::Col<double> state_ = arma::vec(3+2*30,arma::fill::zeros);  // State vector
-  arma::Mat<double> Sigma;  // Covariance matrix
+  arma::Col<double> state_ = arma::vec(3+2*max_obs,arma::fill::zeros);  // State vector
+  arma::Mat<double> Sigma = arma::join_cols(arma::join_rows(arma::Mat<double>(3, 3, arma::fill::zeros), arma::Mat<double>(3, 2 * max_obs, arma::fill::zeros)),
+                                            arma::join_rows(arma::Mat<double>(2 * max_obs, 3, arma::fill::zeros), arma::Mat<double>(2 * max_obs, 2 * max_obs, arma::fill::eye) * 10000));  // Covariance matrix
+  arma::Col<double> z_pred = arma::vec(2*max_obs,arma::fill::zeros);  // Measurement vector
+  arma::Col<double> z_obs = arma::vec(2*max_obs,arma::fill::zeros);  // Measurement vector
+  arma::Mat<double> H = arma::Mat<double>(2*max_obs, 3+2*max_obs, arma::fill::zeros);  // Jacobian matrix
 
 
   std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
   rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr sub_odom_;
   rclcpp::Subscription<visualization_msgs::msg::MarkerArray>::SharedPtr sub_fake_sensor_;
+  rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr pub_estimate_obs_;
   rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr pub_path_;
   rclcpp::TimerBase::SharedPtr timer_;
 };
